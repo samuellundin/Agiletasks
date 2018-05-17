e453d2c021ca64d7f857edfa8d876460a2aeff0b
Samuel Lundin <samuel.lundin@alten.se>
2018-05-17 18:58:17 +0200

    Task CRUD

--- src/main/java/com/agiletasks/controller/TaskController.java
+++ src/main/java/com/agiletasks/controller/TaskController.java
@@ -1,9 +1,11 @@
 package com.agiletasks.controller;
 
+import com.agiletasks.model.TaskModel;
 import com.agiletasks.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/tasks")
@@ -15,4 +17,25 @@ public class TaskController {
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskModel> getTaskById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(taskService.getTaskById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskModel> createTask(@RequestBody TaskModel taskModel) {
+        return new ResponseEntity<>(taskService.createTask(taskModel), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<TaskModel> updateTask(@RequestBody TaskModel taskModel) {
+        return new ResponseEntity<>(taskService.updateTask(taskModel), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable("id") Long id) {
+        taskService.deleteTask(id);
+    }
+
 }
--- src/main/java/com/agiletasks/service/TaskService.java
+++ src/main/java/com/agiletasks/service/TaskService.java
@@ -1,5 +1,8 @@
 package com.agiletasks.service;
 
+import com.agiletasks.entity.Task;
+import com.agiletasks.entity.User;
+import com.agiletasks.model.TaskModel;
 import com.agiletasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +17,27 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
+    public TaskModel getTaskById(Long id) {
+        return new TaskModel(taskRepository.findOne(id));
+    }
+
+    public TaskModel createTask(TaskModel taskModel) {
+        Task task = taskRepository.save(new Task(taskModel));
+        return new TaskModel(task);
+    }
+
+    public TaskModel updateTask(TaskModel taskModel) {
+        Task task = taskRepository.findOne(taskModel.getId());
+        task.setTitle(taskModel.getTitle());
+        task.setDescription(taskModel.getDescription());
+        task.setStatus(taskModel.getStatus());
+        task.setSprintId(taskModel.getSprintId());
+        task.setAssigned(new User(taskModel.getAssigned()));
+        return new TaskModel(taskRepository.save(task));
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.delete(id);
+    }
 
 }
